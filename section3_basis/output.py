0cb75bffd0e2cec2ef35bf27a069567a94400608
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-07 19:19:04 +0900

    split

--- section3_basis/output.py
+++ section3_basis/output.py
@@ -10,3 +10,17 @@ result = math.sqrt(25)
 print(result, type(result))
 
 print('商の整数部分', 20//3)
+
+print("""\
+line1
+line2\
+      """)
+
+word = 'python'
+print(word[0])
+print(word[-1])
+print(word[1:4])
+print(word[:2])
+print(word[2:])
+length = len(word)
+print(length)
