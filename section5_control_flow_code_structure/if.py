08f638fdad8128e3ef44a3e34b07525949b84aa4
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-08 12:15:34 +0900

    not

--- section5_control_flow_code_structure/if.py
+++ section5_control_flow_code_structure/if.py
@@ -8,3 +8,6 @@ elif x == 0:
     print('zero')
 else:
     print('negative')
+
+if x == 10 and y == 5:
+    print(22)
---
+++ section5_control_flow_code_structure/in_not.py
@@ -0,0 +1,8 @@
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+
+if 66 not in y:
+    print('not in')
