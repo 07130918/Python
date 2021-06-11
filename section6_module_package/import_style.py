6de8e3542122c237cbe1ce49e8c97dc5c05036fc
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-11 18:56:36 +0900

    import order

---
+++ section6_module_package/import_style.py
@@ -0,0 +1,21 @@
+# 1.標準ライブラリ
+import collections
+import os
+import sys
+
+# 2. 3rd partyライブラリ
+import termcolor
+
+# 3.チームが開発したもの
+import l_package
+
+# 4.自分が開発したもの
+import config
+
+print(collections.__file__)
+print(os.getcwd())
+print(sys.path)
+print('---------------')
+print(termcolor.__file__)
+print(l_package.__file__)
+print(config.__file__)
