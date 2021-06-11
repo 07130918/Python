560d23bb2b1982b8ba655c0046ac5f13cdb08208
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-11 12:13:40 +0900

    import animal

--- section6_module_package/import_as.py
+++ section6_module_package/import_as.py
@@ -1,11 +1,15 @@
-# import l_package.utils
-# from l_package import utils as u
-from l_package.tools import utils
+# import l_package.tools.utils
+# from l_package.tools import utils as u
 
-from l_package.talk import human
+# from l_package.talk import *
+from l_package.talk import animal, human
+from l_package.tools import utils
 
 result = utils.say_twice("hello")
 print(result)
 
 print(human.sing())
 print(human.cry())
+
+print(animal.sing())
+print(animal.cry())
---
+++ section6_module_package/l_package/talk/animal.py
@@ -0,0 +1,9 @@
+from l_package.tools import utils
+
+
+def sing():
+    return "gwfgrgbgbr"
+
+
+def cry():
+    return utils.say_twice("bfgbhnsfgbgsb")
