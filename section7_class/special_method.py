8293bec181db3332263233e0194cc85d3340d0df
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-13 13:17:53 +0900

    special methods

---
+++ section7_class/special_method.py
@@ -0,0 +1,31 @@
+class Word(object):
+
+    # __があるのが特殊メソッド
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return 'Word!!!!'
+
+    def __len__(self):
+        return len(self.text)
+
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
+
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
+
+
+w = Word('test')
+w2 = Word('test')
+# __str__
+print(w)
+# __len__
+print(len(w))
+# __add__
+print(w + w2)
+# __eq__
+print(id(w))
+print(id(w2))
+print(w == w2)
