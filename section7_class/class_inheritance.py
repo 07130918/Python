410e1547fa9b5e0eaa08a8e81102f61e2f3950e3
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-13 11:17:03 +0900

    setter getter

--- section7_class/class_inheritance.py
+++ section7_class/class_inheritance.py
@@ -11,9 +11,23 @@ class ToyotaCar(Car):
 
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=True):
+    def __init__(self, model='Model S', enable_auto_run=False, pass_word='123'):
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run
+        self._enable_auto_run = enable_auto_run
+        self.pass_word = pass_word
+
+    # getter
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    # setter
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.pass_word == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
 
     def run(self):
         print('super fast')
@@ -31,8 +45,10 @@ print(toyota_car.model)
 toyota_car.run()
 print('-------------------------')
 
-tesla_car = TeslaCar()
+tesla_car = TeslaCar(pass_word='456')
 print(tesla_car.model)
+# setter, getter関数は()なしで呼び出せる(is_enableにTrueが入る)
+tesla_car.enable_auto_run = True
 print(f'Auto run mode: {tesla_car.enable_auto_run}')
 tesla_car.run()
 tesla_car.auto_run()
