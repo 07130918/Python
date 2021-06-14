7a082457b750955282b99b3e7ebf9d9285b87f4a
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-15 08:55:19 +0900

    with statment

--- section8_file_system/file_create.py
+++ section8_file_system/file_create.py
@@ -3,3 +3,10 @@ f.write('test\n')
 print("Hello text file", file=f)
 print('My', 'name', 'is', 'kota', sep='#', end='!', file=f)
 f.close()
+
+# ブロックでファイル作業は行う
+with open('test.txt', 'w') as f:
+    print("------------", file=f)
+    f.write('test\n')
+    print("Hello text file", file=f)
+    print('My', 'name', 'is', 'kota', sep='#', end='!', file=f)
