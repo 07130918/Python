30d2f5d925e028ca246aa3cf90b53484caa3c2eb
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-15 11:41:15 +0900

    dir and file position changed

--- section8_file_system/tar_training.py
+++ section8_file_system/tar_training.py
@@ -1,11 +1,11 @@
 import tarfile
 
 # tarの作成
-with tarfile.open('test.tar.gz', 'w:gz') as tr:
-    tr.add('test_dir')
+with tarfile.open('material/test.tar.gz', 'w:gz') as tr:
+    tr.add('material/test_dir')
 
 # tarの展開
-with tarfile.open('test.tar.gz', 'r:gz') as tr:
+with tarfile.open('material/test.tar.gz', 'r:gz') as tr:
     # tr.extractall(path='test_tar')
-    with tr.extractfile('test_dir/sub_dir/sub_test.txt') as f:
+    with tr.extractfile('material/test_dir/sub_dir/sub_test.txt') as f:
         print(f.read())
--- section8_file_system/zip_training.py
+++ section8_file_system/zip_training.py
@@ -2,16 +2,16 @@ import glob
 import zipfile
 
 # zipの作成
-with zipfile.ZipFile('test.zip', 'w') as z:
-    # z.write('test_dir')
-    # z.write('test_dir/text.txt')
-    for f in glob.glob('test_dir/**', recursive=True):
+with zipfile.ZipFile('material/test.zip', 'w') as z:
+    # z.write('material/test_dir')
+    # z.write('material/test_dir/text.txt')
+    for f in glob.glob('material/test_dir/**', recursive=True):
         print(f)
         z.write(f)
 
 # zipの展開
-with zipfile.ZipFile('test.zip', 'r') as z:
+with zipfile.ZipFile('material/test.zip', 'r') as z:
     # z.extractall('zip_dep')
-    with z.open('test_dir/sub_dir/sub_test.txt') as f:
+    with z.open('material/test_dir/sub_dir/sub_test.txt') as f:
         print('--------------')
         print(f.read())
