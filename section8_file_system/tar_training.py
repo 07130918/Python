2e3174f5cc65046dda324e5020d7f9de3cf205cb
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-15 11:15:10 +0900

    extractfile

--- section8_file_system/tar_training.py
+++ section8_file_system/tar_training.py
@@ -4,4 +4,6 @@ with tarfile.open('test.tar.gz', 'w:gz') as tr:
     tr.add('test_dir')
 
 with tarfile.open('test.tar.gz', 'r:gz') as tr:
-    tr.extractall(path='test_tar')
+    # tr.extractall(path='test_tar')
+    with tr.extractfile('test_dir/sub_dir/sub_test.txt') as f:
+        print(f.read())
