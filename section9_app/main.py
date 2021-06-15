0027d87f0e7aee1e791f8782216ad9f6f333b1fb
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-15 18:01:06 +0900

    csvのからのときの処理

--- section9_app/main.py
+++ section9_app/main.py
@@ -1,3 +1,5 @@
+import csv
+
 from termcolor import colored
 
 
@@ -12,6 +14,15 @@ def get_name():
     return input('Name: ')
 
 
+def recommend():
+    with open('ranking.csv', 'r') as csv_file:
+        if len([row for row in csv.DictReader(csv_file)]) == 0:
+            return
+        for row in csv.DictReader(csv_file):
+            print(row)
+            print(row['Restaurant name'], row['Count'])
+
+
 def ask_restaurant(name):
     print(colored('======================================', 'green'))
     print(colored(f"{name}, which restaurants do you like?", 'green'))
@@ -23,6 +34,14 @@ def get_restaurant_name():
     return input('Restaurant name: ')
 
 
+def write_csv(restaurant_name):
+    with open('ranking.csv', 'a+') as csv_file:
+        fieldnames = ['Restaurant name', 'Count']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'Restaurant name': restaurant_name, 'Count': 1})
+
+
 def close_conversation(name):
     print(colored('======================================', 'green'))
     print(colored(f"Thank you so much! {name}", 'green'))
@@ -31,9 +50,11 @@ def close_conversation(name):
 
 
 def main():
-    name = opening_greeting()
-    ask_restaurant(name)
-    close_conversation(name)
+    # name = opening_greeting()
+    recommend()
+    # restaurant_name = ask_restaurant(name)
+    # write_csv(restaurant_name)
+    # close_conversation(name)
 
 
 if __name__ == '__main__':
