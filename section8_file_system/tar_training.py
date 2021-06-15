5bf53c9c53943977cbc3263cfdff3141d24585bf
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-15 11:33:17 +0900

    zip train

--- section8_file_system/tar_training.py
+++ section8_file_system/tar_training.py
@@ -1,8 +1,10 @@
 import tarfile
 
+# tarの作成
 with tarfile.open('test.tar.gz', 'w:gz') as tr:
     tr.add('test_dir')
 
+# tarの展開
 with tarfile.open('test.tar.gz', 'r:gz') as tr:
     # tr.extractall(path='test_tar')
     with tr.extractfile('test_dir/sub_dir/sub_test.txt') as f:
