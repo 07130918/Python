dc05d2ecbae78f21a7cec6adb071ac9b1f404faa
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-06-18 11:14:17 +0900

    configparser

--- section11_config_logging/configparser_training.py
+++ section11_config_logging/configparser_training.py
@@ -10,7 +10,30 @@ port = 80
 host = 127.0.0.1
 port = 3306
 """
+
+# アプリの設定読み書きの時に使う
+
 import configparser
 
 config = configparser.ConfigParser()
-print(config)
+config['DEFAULT'] = {
+    'debug': True
+}
+config['web_server'] = {
+    'host': '127.0.0.1',
+    'port': 80
+}
+config['db_server'] = {
+    'host': '127.0.0.1',
+    'port': 3306
+}
+
+with open('config.ini', 'w') as config_file:
+    config.write(config_file)
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+print(config['web_server'])
+print(config['web_server']['host'])
+print(config['web_server']['port'])
