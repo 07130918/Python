7419f41deefef13663c7de1d6b1afa121fb6a0ab
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-01 11:29:02 +0900

    add comment

--- section14_test/test_pytest_training.py
+++ section14_test/test_pytest_training.py
@@ -5,6 +5,9 @@ def test_add_num_and_double():
     cal = test_cal.Cal()
     assert cal.add_num_and_double(1, 1) == 4
 
+# class name must start to Test
+# func name must start to test_
+
 
 class TestCal(object):
     def test_add_num_and_double(self):
