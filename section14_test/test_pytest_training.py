f7e88a4eb1d8bcd23d59622dda874799873ef2f4
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-01 11:32:52 +0900

    raise

--- section14_test/test_pytest_training.py
+++ section14_test/test_pytest_training.py
@@ -1,3 +1,4 @@
+import pytest
 import test_cal
 
 
@@ -13,3 +14,8 @@ class TestCal(object):
     def test_add_num_and_double(self):
         cal = test_cal.Cal()
         assert cal.add_num_and_double(1, 1) == 6
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = test_cal.Cal()
+            cal.add_num_and_double(1, 1)
