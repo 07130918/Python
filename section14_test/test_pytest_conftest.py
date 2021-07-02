e2841a15325264d702256c1a74b2d1ab1140e04f
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-02 11:49:54 +0900

    test

--- section14_test/test_pytest_conftest.py
+++ section14_test/test_pytest_conftest.py
@@ -1,8 +1,10 @@
 # 実行コマンド
 # pytest test_pytest_conftest.py --os-name = mac - s
 
-# import pytest
 import os
+import shutil
+
+import pytest
 
 import test_cal
 
@@ -12,7 +14,19 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = test_cal.Cal()
+        cls.test_dir = '/tmp/test_dir'
         cls.test_file_name = 'test.txt'
+
+    @classmethod
+    def teardown_class(cls):
+        if os.path.exists(cls.test_dir):
+            shutil.rmtree(cls.test_dir)
+
+    def test_save_no_dir(self):
+        self.cal.save(self.test_dir, self.test_file_name)
+        test_file_path = os.path.join(self.test_dir, self.test_file_name)
+        print(f'test_file_path: {test_file_path}')
+        assert os.path.exists(test_file_path) is True
 
     def test_add_num_and_double(self, request, csv_file):
         """ request,tmpdirなどの決まったfixture名+自分で作れる
@@ -31,3 +45,7 @@ class TestCal(object):
         test_file_path = os.path.join(tmpdir, self.test_file_name)
         print(f'test_file_path: {test_file_path}')
         assert os.path.exists(test_file_path) is True
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double("1", 1)
