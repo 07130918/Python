0e1055de23205248f3603027b7e6f9bf35ae347a
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-02 10:16:14 +0900

    fixture

--- section14_test/test_pytest_conftest.py
+++ section14_test/test_pytest_conftest.py
@@ -2,6 +2,7 @@
 # pytest test_pytest_conftest.py --os-name = mac - s
 
 # import pytest
+import os
 
 import test_cal
 
@@ -11,6 +12,7 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = test_cal.Cal()
+        cls.test_file_name = 'test.txt'
 
     def test_add_num_and_double(self, request):
         """ 外から受け取る場合引数名は必ずrequest
@@ -22,3 +24,8 @@ class TestCal(object):
         elif os_name == 'windows':
             print('dir')
         assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_save(self, tmpdir):
+        self.cal.save(tmpdir, self.test_file_name)
+        test_file_path = os.path.join(tmpdir, self.test_file_name)
+        assert os.path.exists(test_file_path) is True
