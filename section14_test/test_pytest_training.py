6fd627f18ad18f02a0a18e9bb0cf9a07bbbbaa35
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-02 09:05:32 +0900

    change comment position

--- section14_test/test_pytest_training.py
+++ section14_test/test_pytest_training.py
@@ -1,13 +1,13 @@
 import pytest
 import test_cal
 
+# class name must start to Test
+# func name must start to test_
+
 
 def test_add_num_and_double():
     cal = test_cal.Cal()
     assert cal.add_num_and_double(1, 1) == 4
-
-# class name must start to Test
-# func name must start to test_
 
 
 class TestCal(object):
