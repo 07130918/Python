0170950f2ed9e3f218465bf936aa87c353a6d7f5
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-02 11:07:03 +0900

    fixture

--- section14_test/conftest.py
+++ section14_test/conftest.py
@@ -1,3 +1,19 @@
-# これはpytestで決まっている書き方
+import os
+import pytest
+
+
+# original fixture
+@pytest.fixture
+def csv_file(tmpdir):
+    # return 'csv file!!!'
+    with open(os.path.join(tmpdir, 'test.csv'), 'w+') as c:
+        print('before test')
+        print(f'tmpdir: {tmpdir}')
+        yield c
+        print('after test')
+
+
 def pytest_addoption(parser):
+    """これはpytestで決まっている書き方
+    """
     parser.addoption('--os-name', default='linux')
--- section14_test/test_pytest_conftest.py
+++ section14_test/test_pytest_conftest.py
@@ -14,9 +14,10 @@ class TestCal(object):
         cls.cal = test_cal.Cal()
         cls.test_file_name = 'test.txt'
 
-    def test_add_num_and_double(self, request):
-        """ 外から受け取る場合引数名は必ずrequest
+    def test_add_num_and_double(self, request, csv_file):
+        """ request,tmpdirなどの決まったfixture名+自分で作れる
         """
+        print(csv_file)
         os_name = request.config.getoption('--os-name')
         print(os_name)
         if os_name == 'mac':
@@ -28,5 +29,5 @@ class TestCal(object):
     def test_save(self, tmpdir):
         self.cal.save(tmpdir, self.test_file_name)
         test_file_path = os.path.join(tmpdir, self.test_file_name)
-        print(test_file_path)
+        print(f'test_file_path: {test_file_path}')
         assert os.path.exists(test_file_path) is True
