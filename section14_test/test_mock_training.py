bb290c7f040315b570c5a3019521ef5a82a081d3
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-05 11:12:12 +0900

    mock assert

--- section14_test/test_mock_training.py
+++ section14_test/test_mock_training.py
@@ -8,8 +8,18 @@ class TestSalary(unittest.TestCase):
     def test_calculation_salary(self):
         s = mock_training.Salary(year=2017)
         s.bonus_api.bonus_price = MagicMock(return_value=1)
-        print(s.calculation_salary())
         self.assertEqual(s.calculation_salary(), 101)
+        # bounas_price confirmation
+        s.bonus_api.bonus_price.assert_called()
+        s.bonus_api.bonus_price.assert_called_once()
+        s.bonus_api.bonus_price.assert_called_with(year=2017)
+        self.assertEqual(s.bonus_api.bonus_price.call_count, 1)
+
+    def test_calculation_salary_no_salary(self):
+        s = mock_training.Salary(year=2050)
+        s.bonus_api.bonus_price = MagicMock(return_value=0)
+        self.assertEqual(s.calculation_salary(), 100)
+        s.bonus_api.bonus_price.assert_not_called()
 
 
 if __name__ == '__main__':
