76ec1f484400e6d1728fe65e410328f22bd509db
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-05 13:24:27 +0900

    effect

--- section14_test/test_mock_training.py
+++ section14_test/test_mock_training.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
+from unittest import mock
 
 import mock_training
 
@@ -20,6 +21,71 @@ class TestSalary(unittest.TestCase):
         s.bonus_api.bonus_price = MagicMock(return_value=0)
         self.assertEqual(s.calculation_salary(), 100)
         s.bonus_api.bonus_price.assert_not_called()
+
+    @mock.patch('mock_training.ThirdPartyBonusRestApi.bonus_price', return_value=1)
+    def test_calculation_salary_patch(self, mock_bonus):
+        """ when you use docollator
+            decollator引数にreturn_valueがない場合はコメントのようなコードを書く
+        """
+        # mock_bonus.return_value = 1
+
+        s = mock_training.Salary(year=2017)
+        # salary_price = s.calculation_salary()
+
+        self.assertEqual(s.calculation_salary(), 101)
+        # self.assertEqual(salary_price, 101)
+        mock_bonus.assert_called()
+
+    def test_calculation_salary_patch_with(self):
+        """ when you use with statement
+        """
+        with mock.patch(
+                'mock_training.ThirdPartyBonusRestApi.bonus_price') as mock_bonus:
+            mock_bonus.return_value = 1
+
+            s = mock_training.Salary(year=2017)
+            salary_price = s.calculation_salary()
+
+            self.assertEqual(salary_price, 101)
+            mock_bonus.assert_called()
+
+    def setUp(self):
+        self.patcher = mock.patch('mock_training.ThirdPartyBonusRestApi.bonus_price')
+        self.mock_bonus = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_calculation_salary_patch_with_patcher(self):
+        """ when you use patcher
+        """
+        self.mock_bonus.return_value = 1
+
+        s = mock_training.Salary(year=2017)
+        salary_price = s.calculation_salary()
+
+        self.assertEqual(salary_price, 101)
+
+    def test_calculation_salary_patch_side_effect(self):
+        """ when you use side effect
+        """
+        # def f(year):
+        #     return year * 2
+        # self.mock_bonus.side_effect = f
+        # self.mock_bonus.side_effect = ConnectionRefusedError
+        self.mock_bonus.side_effect = [1, 2, ValueError('Bankrupt!!!')]
+
+        s = mock_training.Salary(year=2017)
+        salary_price = s.calculation_salary()
+        self.assertEqual(salary_price, 101)
+
+        s = mock_training.Salary(year=2018)
+        salary_price = s.calculation_salary()
+        self.assertEqual(salary_price, 102)
+
+        s = mock_training.Salary(year=2019)
+        with self.assertRaises(ValueError):
+            s.calculation_salary()
 
 
 if __name__ == '__main__':
