046fbb6d077836dfa6c8249bcb2519bbdc373ec7
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-06 09:43:53 +0900

    add args

--- section15_parallelization/thread_training.py
+++ section15_parallelization/thread_training.py
@@ -6,22 +6,24 @@ logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
 
 def worker1():
-    logging.debug('start')
+    logging.debug('worker1 start')
     # print(threading.current_thread().getName(), 'start')
     time.sleep(2)
-    logging.debug('end')
+    logging.debug('worker1 end')
     # print(threading.current_thread().getName(), 'end')
 
 
-def worker2():
-    print(threading.current_thread().getName(), 'start')
-    time.sleep(3)
-    print(threading.current_thread().getName(), 'end')
+def worker2(x, y=1):
+    print(threading.current_thread().getName(), ':worker2: start')
+    logging.debug(x)
+    logging.debug(y)
+    time.sleep(2)
+    print(threading.current_thread().getName(), ':worker2: end')
 
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=worker1)
-    t2 = threading.Thread(target=worker2)
+    t1 = threading.Thread(name='rename worker1', target=worker1)
+    t2 = threading.Thread(target=worker2, args=(100, ), kwargs={'y': 200})
     t1.start()
     t2.start()
     print('started')
