ed7c4c5c1c90651c205ea0b0777ff7b059e00139
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-06 10:12:16 +0900

    thread list

---
+++ section15_parallelization/thread_list.py
@@ -0,0 +1,26 @@
+import logging
+import threading
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
+
+
+def worker1():
+    logging.debug('start')
+    time.sleep(1)
+    logging.debug('end')
+
+
+if __name__ == '__main__':
+    # threads = []
+    for _ in range(5):
+        t1 = threading.Thread(target=worker1)
+        t1.setDaemon(True)
+        t1.start()
+        # threads.append(t1)
+        print(threading.enumerate())
+    for thr in threading.enumerate():
+        if thr is threading.current_thread():
+            print(thr)
+            continue
+        thr.join()
