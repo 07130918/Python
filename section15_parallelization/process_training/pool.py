ae93f5b8729397bfc00353d148afaa463c1362f5
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-07 10:52:25 +0900

    pool

---
+++ section15_parallelization/process_training/pool.py
@@ -0,0 +1,24 @@
+import logging
+import multiprocessing
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+
+def worker1(i):
+    logging.debug('worker1 start')
+    time.sleep(3)
+    logging.debug('worker1 end')
+    return i
+
+
+if __name__ == '__main__':
+    # poolで走らせるprocessの数を決める
+    with multiprocessing.Pool(2) as p:
+        p1 = p.apply_async(worker1, (100, ))
+        p2 = p.apply_async(worker1, (100, ))
+        p3 = p.apply_async(worker1, (100, ))
+        logging.debug('executed')
+        logging.debug(p1.get())
+        logging.debug(p2.get())
+        logging.debug(p3.get())
