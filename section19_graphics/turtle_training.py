d878f6ed8416d1ec9941d9c2aca08c670da15bef
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-16 10:44:28 +0900

    turtle

--- section19_graphics/turtle_training.py
+++ section19_graphics/turtle_training.py
@@ -1,1 +1,26 @@
+import turtle
 
+turtle.pencolor('green')
+
+for i in range(60):
+    turtle.forward(30)
+    turtle.left(360 / 3 + 10)
+turtle.done()
+
+# turtle.color('red', 'yellow')
+
+# turtle.begin_fill()
+# for i in range(5 * 3):
+#     turtle.forward(100 + i * 10)
+#     turtle.right(360 / 5 * 2)
+# turtle.end_fill()
+
+# turtle.done()
+
+# turtle.begin_fill()
+# for _ in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.end_fill()
+
+# turtle.done()
