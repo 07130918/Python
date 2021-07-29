0b1d7da80edaed51efb66052a4ad410d9eb0568d
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-07-29 12:38:16 +0900

    zeromq

---
+++ section21_queuing_system/zeromq_client.py
@@ -0,0 +1,13 @@
+from email import message
+from aem import con
+import zmq
+
+context = zmq.Context()
+# sock = context.socket(zmq.PULL)
+sock = context.socket(zmq.SUB)
+sock.setsockopt(zmq.SUBSCRIBE, b'sub1:')
+sock.connect("tcp://127.0.0.1:5690")
+
+while True:
+    message = sock.recv()
+    print(f"Received: {message.decode()}")
