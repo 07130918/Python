ba5103542bacdf38b2e9acf8380e1d69f64d8ade
佐藤宏太 <satokota@satoukoutanoMacBook-Air.local>
2021-08-08 07:35:38 +0900

    2 line

--- section22_asyncio/asyncio_call.py
+++ section22_asyncio/asyncio_call.py
@@ -2,9 +2,11 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
+
 def hello(name, loop):
     print(f'Hello {name}')
     loop.stop()
+
 
 loop.call_later(2, hello, 'Mike', loop)
 loop.call_soon(hello, 'Kota', loop)
